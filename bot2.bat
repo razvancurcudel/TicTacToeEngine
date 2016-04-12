61495b3cafb733311acbef00010881212bfd81e0
RazvanRzv <ioan.curcudel@cti.pub.ro>
2016-04-12 16:35:33 +0300

    added scripts

---
+++ bot2.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp "E:\java projects\TTT\bin" bot.BotStarter 2> "E:\java projects\TTT\err2.txt"
---
+++ runbots.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp "E:\java projects\TicTacToeEngine\bin" tictactoe.TicTacToe "C:\\Users\\vKint\\Desktop\\bot1.bat" "C:\\Users\\vKint\\Desktop\\bot2.bat" 2>dump.txt
